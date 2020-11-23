6549fd3acc618996d15ae22c99af0478690ecbd7
Huynh Kim Phat <hkimphat2012@gmail.com>
2020-11-23 22:04:20 +0700

    Adding SQL

---
+++ Project_1/contact.sql
@@ -0,0 +1,26 @@
+USE MASTER
+CREATE DATABASE CONTACT_BOOK
+
+USE CONTACT_BOOK
+GO
+
+
+CREATE TABLE PEOPLE
+(
+    id      INT           IDENTITY PRIMARY KEY,
+    name    NVARCHAR(100) NOT NULL DEFAULT N'Unknown name',
+    address NVARCHAR(100) NOT NULL DEFAULT N'Unknown address',
+    phone   NVARCHAR(100) NOT NULL DEFAULT N'Unknown phone',
+    email   NVARCHAR(100) NOT NULL DEFAULT N'Unknown email',
+)
+
+INSERT PEOPLE
+    (name,address,phone,email)
+VALUES('Phat', '154 GV', '0944651790', '19521992@gm.uit.vn' )
+
+INSERT PEOPLE
+    (name,address,phone,email)
+VALUES('Quy', '108 Q9', '0935589947', '19522108@gm.uit.vn' )
+
+SELECT *
+FROM PEOPLE
