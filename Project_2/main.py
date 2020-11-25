228908d731d1934487368c2bfbc82eff73fb75a8
Huynh Kim Phat <hkimphat2012@gmail.com>
2020-11-25 09:25:43 +0700

    Add main Project2

---
+++ Project_2/main.py
@@ -0,0 +1,54 @@
+story_1 = """
+The story of {0} tells of a kindhearted young woman who was treated cruelly by her stepmother and sisters, 
+but, never the less, kept a humble attitude.
+
+One day, The king's name is {1} and he decided to throw a ball and invited all the young maidens in the kingdom.
+While {0}'s sisters made her help them get ready for the ball, not once did they ask her if she would like to go with them.
+
+Once they left, her Fairy Godmother appeared and helped {0} go to the ball with a bit of magic that would only last until midnight.
+At the ball, {0} caught the eye of the prince, as she was the most beautiful girl there, and they danced all night.
+
+When midnight came, {0} had to leave the ball, and in her hurry, one of her glass slippers fell off her feet.
+The prince found this slipper and vowed to marry the girl who the slipper belonged to.
+
+The prince went from house to house, looking for the girl who’s foot fit the slipper, and he reached {0}'s house.
+Though {0}'s stepsisters and stepmother tried to keep her from trying it on, the glass slipper was a perfect fit,
+and she was soon married to the prince and lived happily ever after.
+
+This is a wonderful story that captures how keeping a humble attitude will reap its rewards.
+"""
+
+story_2 = """
+Originating in France, this is the story of {0}, a beautiful peasant girl who took the place of her father, 
+when he was taken prisoner by a fierce beast.
+
+While things were uncomfortable and frightening for {0} at first, she soon grew fond of the beast, 
+as he had done nothing but treat her with kindness. When {0} found out her father was sick, 
+she begged the beast to let her go to him and promised to return, but she was held up by the evil Gaston, 
+a famous hunter from the village who wanted to marry {0}. When the village found out about the beast, 
+they vowed to kill him and stormed his castle. 
+
+Though he nearly died, he was saved and turned into a handsome prince because of {0}’s love for him. 
+It turns out that he had been a prince who, along with his entire household, 
+was cursed by a witch because he did not treat her with kindness.
+ {0} and the Prince marry, and live a happy and peaceful life together.
+
+From the prince’s curse, children can learn about the importance of being kind and that if they do not, 
+they will suffer bad consequences. From {0}, we learn to value what is in a person’s heart, 
+rather than their outward appearance.
+
+"""
+
+
+def menu():
+    print("Menu: ")
+    print("Story 1: Cinderella")
+    print("Story 2: Beauty And The Beast")
+    print("Story 3: Sleeping Beauty")
+    print("Story 4: Rapunzel")
+    print("Story 5: Snow Town And The Seven Dwarfs")
+    print("Story 6: The little Mermaid")
+    print("Story 7: Goldilocks and the Three Bears")
+    print("Story 8: The Princess and the Pea")
+    print("Story 9: The Ugly Duckling")
+    print("Story 10: The Frog Prince")
