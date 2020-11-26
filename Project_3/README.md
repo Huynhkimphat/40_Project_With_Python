a67fd60dfc5034956cb46a3ede34414cd3ec3577
Huynh Kim Phat <hkimphat2012@gmail.com>
2020-11-26 12:00:41 +0700

    Add Project_3

---
+++ Project_3/README.md
@@ -0,0 +1,15 @@
+# Project 3
+
+## Number Guessing
+
+### </br>
+
+#### Description
+
+* This is a simple project but useful for entertainment
+
+* App will random a number and you have to choose the correct number
+
+* If your choice is not correct, you will receive some hint to minimize your range of choice
+
+* You just have 10 points and 1 hint will be minus 1 point
---
+++ Project_3/main.py
@@ -0,0 +1,26 @@
+import random
+
+bot_result = random.randint(0, 100)
+
+
+def game_start():
+    your_choice = int(input())
+    if your_choice < bot_result:
+        print("Your choice is smaller than result!!!")
+        return 0
+    if your_choice > bot_result:
+        print("Your choice is higher than result!!!")
+        return 0
+    print("Bingo!!!")
+    return 1
+
+
+if __name__ == "__main__":
+    your_score = 10
+    print(bot_result)
+    while(game_start() == 0 and your_score != 0):
+        your_score -= 1
+    if your_score != 0:
+        print("YOUR SCORE: " + str(your_score))
+    else:
+        print("You lose the game \nThe answer is " + str(bot_result))
